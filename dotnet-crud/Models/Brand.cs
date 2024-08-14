b9a2fdab87e4ab5992189593af172ced264e32b2
Imthadh Ahamed <143585845+imthadh-ahamed@users.noreply.github.com>
2024-08-14 09:44:16 +0530

    Create the model

---
+++ dotnet-crud/Models/Brand.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dotnet_crud.Models
+{
+    public class Brand
+    {
+        [Key]
+        public Guid ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int EstablishedYear { get; set; }
+        public string BrandLogo { get; set; }
+    }
+}
