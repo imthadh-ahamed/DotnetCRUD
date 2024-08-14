da7b72741a2436d40d71ec4e6b921b25bdcb37a5
Imthadh Ahamed <143585845+imthadh-ahamed@users.noreply.github.com>
2024-08-14 09:47:34 +0530

    Add ApplicationDBContext for managing database interactions

---
+++ dotnet-crud/Data/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using dotnet_crud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_crud.Data
+{
+    // This class will be used to interact with the database
+    public class ApplicationDBContext:DbContext
+    {
+        // This constructor will be used to inject the options to the base class
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+            
+        }
+
+        // This DbSet will be used to interact with the Brand table
+        public DbSet<Brand> Brand { get; set; }
+    }
+}
